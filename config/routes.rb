ab2a277644eeaa300a07154b805bddda171fc596
Tecia Vailati <teciavailati@gmail.com>
2019-05-17 12:13:05 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
